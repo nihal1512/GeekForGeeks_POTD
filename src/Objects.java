90aaf5d3bdfae63c8f729e89d2d21a3c72f639cb
Shubham Mishra <shubham.mishra_cs20@gla.ac.in>
2021-10-12 00:31:34 +0530

    Method Calling

---
+++ src/Objects.java
@@ -0,0 +1,16 @@
+public class Objects {
+    int i=10;
+    int j=20;
+    }
+
+
+class B{
+    public static void main(String[] args) {
+        Objects a1=new Objects();
+        System.out.println("hey!");
+
+
+
+    }
+
+}
