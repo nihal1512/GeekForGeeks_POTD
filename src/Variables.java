d1b5d12aee778da7dcaa453c9e8182b33bd0e81a
Shubham Mishra <shubham.mishra_cs20@gla.ac.in>
2021-10-11 13:28:57 +0530

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    int i=10;
+    static int j=20;
+
+
+}
+class B{
+    public static void main(String[] args) {
+        Variables v1=new Variables();
+        System.out.println(Variables.j);
+        System.out.println(v1.j);//Static members can be called using class name reference as well as address name reference
+    }
+}
