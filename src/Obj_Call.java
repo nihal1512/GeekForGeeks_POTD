a8c302dce3f5da57b1df3899974bca25734e6e93
Shubham Mishra <shubham.mishra_cs20@gla.ac.in>
2021-10-12 00:44:47 +0530

    Method Calling

--- src/Obj_Call.java
+++ src/Obj_Call.java
@@ -1,7 +1,7 @@
 public class Obj_Call {
     public static void main(String[] args) {
        test1 p1=new test1();
-        p1.Hey();//No of times you call methods it will equal no of times
+        p1.Hey();//No of times you call methods it will equal no of times print
         p1.Hey();
         p1.Hey();
     }
